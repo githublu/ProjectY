7ecd3d134c40cbdcdcbf7e17afce606fc1111132
githublu <yilu331@gmail.com>
2017-08-20 15:05:21 -0700

    bug fix + classification mgr

--- python/Models/MLPClassifierModel.py
+++ python/Models/MLPClassifierModel.py
@@ -7,12 +7,16 @@ class MLPClassifierModel(Model):
     def __init__(self, parameter):
         Model.__init__(self, "MLPClassifierModel")
         self.parameter = parameter
-        self.model = MLPClassifier(solver=self.parameter["solver"], alpha=self.parameter["alpha"], hidden_layer_sizes=literal_eval(self.parameter["hidden_layer_sizes"]), random_state=self.parameter["random_state"])
+        self.if_continue = True
+        self.solver_index = self.parameter["solver_index"]
+        self.model = MLPClassifier(solver=self.parameter["solver"][self.solver_index], alpha=self.parameter["alpha"],
+                                   hidden_layer_sizes=literal_eval(self.parameter["hidden_layer_sizes"]), random_state=self.parameter["random_state"])
 
     def set_parameter(self, parameter):
         self.parameter = parameter
         try:
-            self.model = MLPClassifier(solver=self.parameter["solver"], alpha=self.parameter["alpha"], hidden_layer_sizes=literal_eval(self.parameter["hidden_layer_sizes"]), random_state=self.parameter["random_state"])
+            self.model = MLPClassifier(solver=self.parameter["solver"][self.solver_index], alpha=self.parameter["alpha"],
+                                       hidden_layer_sizes=literal_eval(self.parameter["hidden_layer_sizes"]), random_state=self.parameter["random_state"])
         except IndexError:
             log_error(IndexError)
 
@@ -29,11 +33,11 @@ class MLPClassifierModel(Model):
         return dataset
 
     def tune(self):
-        self.parameter["hidden_layer_sizes"] = (101, 101, 101, 101)
+        self.solver_index += 1
         try:
-            self.model = MLPClassifier(solver=self.parameter["solver"], alpha=self.parameter["alpha"],
+            self.model = MLPClassifier(solver=self.parameter["solver"][self.solver_index], alpha=self.parameter["alpha"],
                                        hidden_layer_sizes=literal_eval(self.parameter["hidden_layer_sizes"]),
                                        random_state=self.parameter["random_state"])
         except IndexError:
-            log_error(IndexError)
+            self.if_continue = False
         return
