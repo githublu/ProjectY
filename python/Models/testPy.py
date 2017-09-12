952b1f24e354a610e5de58a834f63e3bdce24322
Yi Lu <yilu@Yis-MacBook-Pro.local>
2017-09-11 21:09:56 -0700

    cleanrepo

--- python/Models/testPy.py
+++
@@ -1,3 +0,0 @@
-import sys,os
-fn = os.path.join(os.path.dirname(__file__), 'my_file')
-print(fn)
--- python/playground/sleep.py
+++
@@ -1,8 +0,0 @@
-import datetime
-import sys
-from time import sleep
-
-print("Before sleep")
-sleep(2)
-print("After sleep")
-
